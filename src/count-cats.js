2f943b56759c87ee108ff06735e446af05113087
Viktoryia Tsialiatnikava <boriisovvna@gmail.com>
2025-01-06 21:47:28 +0300

    Changed4

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,12 +14,11 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
+function countCats(/* backyard */) {
   throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }
   
-
 module.exports = {
   countCats
 };
--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,11 +11,14 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(n) {
+  const str = String(n).split('').map(Number);
+  return Math.max(...str.map((x,i)=>{
+    const newStr = str.slice(); 
+    newStr.splice(i,1); 
+    return Number( newStr.join(''))
+  }))
 }
-
 module.exports = {
   deleteDigit
 };
