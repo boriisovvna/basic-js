ed10d81106860e33e0d18dd8bff5ee8d691d8b10
Viktoryia Tsialiatnikava <boriisovvna@gmail.com>
2025-01-06 19:15:23 +0300

    Changed2

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,7 +14,10 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) 
+function countCats(/* matrix */) {
+  throw new NotImplementedError('Not implemented');
+  // remove line with error and write your code here
+}
   
 
 module.exports = {
